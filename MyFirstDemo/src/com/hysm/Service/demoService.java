234277be5020963c866c0a725f756f0c38fe9dce
yxl <1525972156@qq.com>
2019-04-10 16:55:36 +0800

    commit

--- MyFirstDemo/src/com/hysm/Service/demoService.java
+++ MyFirstDemo/src/com/hysm/Service/demoService.java
@@ -2,7 +2,7 @@ package com.hysm.Service;
 
 public class demoService {
     public  String demosevice(){
-        String a = "aaaaa";
+        String a = "aaaaa=====";
         String b = "bbbbb";
         return a+b;
     }
